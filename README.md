8d15ee6979ae4edca0c62941c8112dc53a64ef13
Maciej Krüger <mkg20001@gmail.com>
2019-10-03 19:20:03 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# read-dataurl
+
+A tiny package to read dataURLs
+
+# Usage
+
+```
+const readDataurl = require('read-dataurl')
+
+const data = readDataurl('data:application/json;base64,e30K')
+```
---
+++ src/index.js
@@ -0,0 +1,55 @@
+'use strict'
+
+function readBlob (str) {
+    let i = 5 // skip data;
+
+    let mime = ''
+    let enc = ''
+    let data = ''
+
+    let block = 'mime'
+
+    while (str.length > i) {
+      let cur = str[i]
+
+      switch(block) {
+        case 'mime': {
+          if (cur === ';') {
+            block = 'enc'
+          } else {
+            mime += cur
+          }
+
+          i++
+
+          break
+        }
+        case 'enc': {
+          if (cur === ',') {
+            block = 'data'
+          } else {
+            enc += cur
+          }
+
+          i++
+
+          break
+        }
+        case 'data': {
+          data += cur
+          i++
+
+          break
+        }
+        default: {
+          throw new TypeError('d')
+        }
+      }
+    }
+
+    return {
+      encoding: enc,
+      mime,
+      data
+    }
+  }
